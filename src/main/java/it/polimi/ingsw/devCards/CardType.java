ff6286f886f5e7c4ad1f49468490ee73d1db03d4
emadens <emanuele1.baldelli@mail.polimi.it>
2021-04-14 20:03:03 +0200

    Added constructor to CardType.

--- src/main/java/it/polimi/ingsw/devCards/CardType.java
+++ src/main/java/it/polimi/ingsw/devCards/CardType.java
@@ -3,8 +3,11 @@ package it.polimi.ingsw.devCards;
 import java.util.HashSet;
 
 public class CardType {
-    //private CardColour colour;
-    //private HashSet<CardLevel> levelSet;
+    private CardColour colour;
+    private HashSet<CardLevel> levelSet;
 
-
+    public CardType(CardColour colour, HashSet<CardLevel> levelSet){
+        this.colour=colour;
+        this.levelSet=levelSet;
+    }
 }
