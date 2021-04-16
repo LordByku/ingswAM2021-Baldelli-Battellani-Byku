510941085b878db0f27b88ba257ab091c250b1ac
Paolo <paolo.battellani@mail.polimi.it>
2021-04-16 11:14:47 +0200

    Added Tests and Javadoc for ConversionEffectArea

--- src/main/java/it/polimi/ingsw/playerBoard/ConversionEffectArea.java
+++ src/main/java/it/polimi/ingsw/playerBoard/ConversionEffectArea.java
@@ -1,21 +1,46 @@
 package it.polimi.ingsw.playerBoard;
 
 import it.polimi.ingsw.leaderCards.ConversionEffect;
+import it.polimi.ingsw.leaderCards.InvalidConversionEffectException;
 import it.polimi.ingsw.resources.ChoiceSet;
 
 import java.util.HashSet;
 
+/**
+ * ConversionEffectArea is a container for ConversionEffects in a Board
+ */
 public class ConversionEffectArea {
+    /**
+     * conversionEffects is the set of ConversionEffects contained
+     * in this ConversionEffectArea
+     */
     private HashSet<ConversionEffect> conversionEffects;
 
+    /**
+     * The constructor initializes conversionEffects to an empty set
+     */
     public ConversionEffectArea() {
         conversionEffects = new HashSet<>();
     }
 
-    public void addConversionEffect(ConversionEffect effect){
+    /**
+     * addConversionEffect adds a new ConversionEffect to this ConversionEffectArea
+     * @param effect The ConversionEffect to add
+     * @throws InvalidConversionEffectException effect is null
+     */
+    public void addConversionEffect(ConversionEffect effect) throws InvalidConversionEffectException {
+        if(effect == null) {
+            throw new InvalidConversionEffectException();
+        }
         conversionEffects.add(effect);
     }
 
+    /**
+     * getConversionEffects returns a ChoiceSet where possible choices are
+     * the ConversionEffects provided by this ConversionEffectArea
+     * @return A ChoiceSet containing the ConversionEffects in this
+     * ConversionEffectArea as choices
+     */
     public ChoiceSet getConversionEffects() {
         ChoiceSet choiceSet = new ChoiceSet();
 
