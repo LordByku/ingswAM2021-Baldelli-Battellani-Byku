4e4cd9670a99a8eb9709899bbe758793a75ee6a9
LordByku <andrea.byku@mail.polimi.it>
2021-04-16 13:00:36 +0200

    Added DevCardTest and CardTypeSetTest

--- src/test/java/it/polimi/ingsw/devCardsTest/DevCardTest.java
+++ src/test/java/it/polimi/ingsw/devCardsTest/DevCardTest.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.devCardsTest;
 
 import it.polimi.ingsw.devCards.*;
+import it.polimi.ingsw.playerBoard.Board;
 import it.polimi.ingsw.resources.ConcreteResource;
 import it.polimi.ingsw.resources.resourceSets.*;
 import org.junit.Test;
@@ -46,6 +47,89 @@ public class DevCardTest {
             assertEquals(CardLevel.I,devCard1.getLevel());
         }
     }
-    //TODO
+
+    @Test
+    public void playTest () {
+        Board board1 = new Board();
+        Board board2 = new Board();
+        CardType cardType1 = new CardType(CardColour.BLUE);
+
+        cardType1.addLevel(CardLevel.I);
+        cardType1.addLevel(CardLevel.II);
+
+        CardType cardType2 = new CardType(CardColour.YELLOW);
+        CardType cardType3 = new CardType(CardColour.PURPLE) ;
+
+        cardType2.addLevel(CardLevel.III);
+        cardType2.addLevel(CardLevel.I);
+
+        CardTypeSet cardTypeSet1 = new CardTypeSet();
+        CardTypeSet cardTypeSet2 = new CardTypeSet();
+
+        cardTypeSet1.add(cardType1);
+        cardTypeSet1.add(cardType2);
+
+        cardTypeSet2.add(cardType3);
+
+        DevCard devCard1;
+        DevCard devCard2;
+        DevCard devCard3;
+        DevCard devCard4;
+        DevCard devCard5;
+
+        CardColour blue = CardColour.BLUE;
+        CardColour yellow = CardColour.YELLOW;
+
+        ConcreteResourceSet concreteResourceSet1 = new ConcreteResourceSet();
+        concreteResourceSet1.addResource(ConcreteResource.COIN, 2);
+        ConcreteResourceSet concreteResourceSet2 = new ConcreteResourceSet();
+        concreteResourceSet2.addResource(ConcreteResource.STONE, 2);
+        concreteResourceSet2.addResource(ConcreteResource.SHIELD);
+
+        ConcreteResourceSet concreteResourceSet3 = new ConcreteResourceSet();
+        concreteResourceSet3.addResource(ConcreteResource.SHIELD);
+        ConcreteResourceSet concreteResourceSet4 = new ConcreteResourceSet();
+        concreteResourceSet4.addResource(ConcreteResource.SERVANT);
+
+
+        ChoiceResourceSet choiceResourceSet1 = new ChoiceResourceSet();
+        choiceResourceSet1.addResource(ConcreteResource.SHIELD);
+        ChoiceResourceSet choiceResourceSet2 = new ChoiceResourceSet();
+        choiceResourceSet2.addResource(ConcreteResource.SERVANT);
+
+        SpendableResourceSet input1 = new SpendableResourceSet(choiceResourceSet1);
+        ObtainableResourceSet output1 = new ObtainableResourceSet(choiceResourceSet2);
+        ProductionDetails details1 = new ProductionDetails(input1,output1);
+
+        devCard1 = new DevCard(concreteResourceSet1,blue,CardLevel.I,details1,5);
+        devCard2 = new DevCard(concreteResourceSet2,yellow,CardLevel.I,details1,6);
+        devCard3 = new DevCard(concreteResourceSet1,blue,CardLevel.II,details1,3);
+        devCard4 = new DevCard(concreteResourceSet2,yellow,CardLevel.III,details1,1);
+        devCard5 = new DevCard(concreteResourceSet2,CardColour.PURPLE,CardLevel.II,details1,1);
+
+        board1.addResourcesToStrongbox(concreteResourceSet2);
+        board1.addResourcesToStrongbox(concreteResourceSet1);
+
+        assertTrue(devCard1.canPlay(board1,0));
+        assertFalse(devCard3.canPlay(board1,0));
+
+        assertTrue(board1.containsResources(concreteResourceSet3));
+        devCard1.play(board1,0);
+        assertSame(board1.getCards().get(0).getLevel(), devCard1.getLevel());
+        assertSame(board1.getCards().get(0).getColour(), devCard1.getColour());
+        assertSame(board1.getCards().get(0).getReqResources().getResourceType().getResource(), devCard1.getReqResources().getResourceType().getResource());
+        assertSame(board1.getCards().get(0).getPoints(), devCard1.getPoints());
+        assertSame(board1.getCards().get(0).getProductionPower().getInput().getResourceSet().getResources().get(0).getResource(),devCard1.getProductionPower().getInput().getResourceSet().getResources().get(0).getResource());
+
+        assertFalse((devCard2.canPlay(board1,0)));
+        assertTrue(devCard2.canPlay(board1,1));
+        devCard2.play(board1,1);
+
+        assertSame(board1.getCards().get(1).getLevel(), devCard2.getLevel());
+        assertSame(board1.getCards().get(1).getColour(), devCard2.getColour());
+        assertSame(board1.getCards().get(1).getPoints(), devCard2.getPoints());
+
+
+    }
 
 }
