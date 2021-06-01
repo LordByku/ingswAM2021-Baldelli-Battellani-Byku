50870687e35e807abaaa2a5690268b2242599f53
emadens <emanuele1.baldelli@mail.polimi.it>
2021-06-01 11:16:46 +0200

    Created GameView form

---
+++ src/main/java/it/polimi/ingsw/view/gui/windows/GameView.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.view.gui.windows;
+
+import javax.swing.*;
+
+public class GameView {
+    private JPanel panel;
+    private JPanel board3;
+    private JPanel board1;
+    private JPanel board4;
+    private JPanel board2;
+    private JPanel markets;
+    private JPanel marbleMarket;
+    private JPanel cardMarket;
+    private JLabel nickname3;
+    private JLabel nickname1;
+    private JLabel nickname4;
+    private JLabel nickname2;
+    private JButton viewBoard3Button;
+    private JButton viewBoard4Button;
+    private JButton viewBoard1Button;
+    private JButton viewBoard2Button;
+    private JButton button1;
+
+}
